aa9e2b33bfcbd625bcddc4026b580978fc5f02d9
Ginue <ginue02@uw.edu>
2017-04-13 14:21:01 -0700

    completed some of the problems in exercise 1 for the participation assignment

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,19 +7,18 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+require(fueleconomy)
 
 # You should have have access to the `vehicles` data.frame
-
+View(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
-
+vehicles.1997 <- vehicles[vehicles$year == 1997,]
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(vehicles.1997$model)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+new.dataframe <- vehicles[vehicles$drive == '2-Wheel Drive' && vehicles$] 
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 
